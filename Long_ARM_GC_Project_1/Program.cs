062c9a7fbf6ab5e8b6a4a413d031812c52560d2d
mdinicola <the.m.dinicola@gmail.com>
2021-05-20 18:59:49 -0400

    initial commit

---
+++ Long_ARM_GC_Project_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Long_ARM_GC_Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
