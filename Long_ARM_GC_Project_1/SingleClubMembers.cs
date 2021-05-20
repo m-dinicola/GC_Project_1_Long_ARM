bd283c7d0ffc6ea98985c0a994ff9f05ccb8fb58
Ryan-D-Carter <RyanCarterMI@gmail.com>
2021-05-20 19:18:42 -0400

    Ryans first commit

---
+++ Long_ARM_GC_Project_1/SingleClubMembers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Long_ARM_GC_Project_1
+{
+    class SingleClubMembers : Member
+    {
+        public SingleClubMembers(int id, string name, Club club /*Club class to be added*/) : base(id, name)
+        {
+            ID = id;
+            Name = name;
+            Club
+        }
+
+    }
+}
