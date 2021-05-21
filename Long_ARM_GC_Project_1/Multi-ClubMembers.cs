baa1a8947935957bc05cb2a414d8721c1dc63879
Ryan-D-Carter <RyanCarterMI@gmail.com>
2021-05-20 20:07:27 -0400

    Ryan changes

--- Long_ARM_GC_Project_1/Multi-ClubMembers.cs
+++ Long_ARM_GC_Project_1/Multi-ClubMembers.cs
@@ -4,7 +4,19 @@ using System.Text;
 
 namespace Long_ARM_GC_Project_1
 {
-    class Multi_ClubMembers
+    class Multi_ClubMembers : Member
     {
+        public Multi_ClubMembers(int id, string name) : base(id, name)
+        {
+            ID = id;
+            Name = name;
+        }
+
+        int membershipPoints = 0;
+
+        public override void CheckIn()
+        {
+            membershipPoints++;
+        }
     }
 }
