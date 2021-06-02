b980518e8f05c9aa0adb251e4ad8ed3618a11962
anne <amlovell2@outlook.com>
2021-06-02 18:41:20 -0400

    minor update

--- Long_ARM_GC_Project_1/MemberControl.cs
+++ Long_ARM_GC_Project_1/MemberControl.cs
@@ -6,5 +6,29 @@ namespace Long_ARM_GC_Project_1
 {
     class MemberControl
     {
+    public void CheckInMember()
+        {
+            //call memberSelect to checkin members
+            MemberDB checkin = new MemberDB(memberSelect);
+
+            if (Member == memberSelect)   //check if member is listed in database
+            {
+                if (Member == SingleClubMembers) //check if single club or multi-club member
+                {
+                    if ()  //single club member-  verify club
+                    {
+
+                    }
+                }
+                else   //multiclub member
+                {
+                    //add membership points 
+                }
+            }
+            else
+            {
+                //create new member
+            }
+        }
     }
 }
