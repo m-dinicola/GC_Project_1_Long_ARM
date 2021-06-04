8a2358770c7d5f97a9927ebb429e7aaed50ba60b
Ryan-D-Carter <RyanCarterMI@gmail.com>
2021-06-03 21:27:14 -0400

    Changes to BillView.cs

--- Long_ARM_GC_Project_1/BillView.cs
+++ Long_ARM_GC_Project_1/BillView.cs
@@ -9,15 +9,15 @@ namespace Long_ARM_GC_Project_1
         public static void DisplayBill(Member member)
         {
             Bill newBill = new Bill();
-            Console.WriteLine($"This is the bill of fees for {member.Name}:\nMonthly Plan - {newBill.MonthlyPlan}" +
-                $"\nEquipment - {newBill.Equipment}\nSpa - {newBill.Spa}\nClasses - {newBill.Classes}\nParking - {newBill.Parking}");
+            Console.WriteLine($"This is the bill of fees for {member.Name}:\nMonthly Plan - ${newBill.MonthlyPlan}" +
+                $"\nEquipment - ${newBill.Equipment}\nSpa - ${newBill.Spa}\nClasses - ${newBill.Classes}\nParking - ${newBill.Parking}");
         }
 
         public static void DisplayBill(MultiClubMembers member)
         {
             Bill newBill = new Bill();
-            Console.WriteLine($"This is the bill of fees for {member.Name}:\nMonthly Plan - {newBill.MonthlyPlan}" +
-                $"\nEquipment - {newBill.Equipment}\nSpa - {newBill.Spa}\nClasses - {newBill.Classes}\nParking - {newBill.Parking}\n");
+            Console.WriteLine($"This is the bill of fees for {member.Name}:\nMonthly Plan - ${newBill.MonthlyPlan}" +
+                $"\nEquipment - ${newBill.Equipment}\nSpa - ${newBill.Spa}\nClasses - ${newBill.Classes}\nParking - ${newBill.Parking}\n");
             Console.WriteLine($"Membership Points - {member.MembershipPoints}");
         }
     }
